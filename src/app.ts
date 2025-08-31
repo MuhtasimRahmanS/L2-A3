839cd01505fb1bb90436230c0c8c2118d70a9da4
Muhtasim Rahman <155262460+MuhtasimRahmanS@users.noreply.github.com>
2025-08-31 18:24:37 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from "express";
 import bookRouter from "./module/book/book.router";
 import borrowRouter from "./module/borrow/borrow.router";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://l2-a4.vercel.app"],
+  })
+);
 app.use(express.json())
 app.use('/api/books',bookRouter)
 app.use('/api/borrow',borrowRouter)
